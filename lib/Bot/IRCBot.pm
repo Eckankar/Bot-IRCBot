a5d1995e6ec9850e1a594877258bd962c1a62bed
Sebastian Paaske Tørholm <Eckankar+GitHub@gmail.com>
2009-06-19 16:08:13 -0400

    Initial commit.

---
+++ lib/Bot/IRCBot.pm
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+package IRCBot;
+use Moose;
+use YAML 'LoadFile';
+
+extends 'Bot::BasicBot';
+
+do {
+    my $conf;
+    sub conf {
+        $conf = LoadFile('etc/ircbot.conf')
+            unless ($conf);
+
+        return $conf->{$_[0]} if @_;
+        return wantarray ? %$conf : $conf;
+    }
+};
+
+1;
+
