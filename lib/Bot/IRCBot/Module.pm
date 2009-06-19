268a16d811ca90873ab634a37746e19f89358df9
Sebastian Paaske Tørholm <Eckankar+GitHub@gmail.com>
2009-06-19 18:16:04 -0400

    Fleshed out preliminary structure of Module.pm
    modified:   lib/Bot/IRCBot/Module.pm

--- lib/Bot/IRCBot/Module.pm
+++ lib/Bot/IRCBot/Module.pm
@@ -2,7 +2,45 @@
 package Bot::IRCBot::Module;
 use Moose;
 
-# This will be an interface for a module.
+has 'name' => (
+    is      => 'r',
+    isa     => 'Str',
+    default => 'Default Module Name (override this!)',
+);
+
+has 'loaded' => (
+    is      => 'rw',
+    isa     => 'Bool',
+    default => 0,
+);
+
+sub init {
+    my $self = shift;
+}
+
+sub load {
+    my $self = shift;
+
+    return 0 if $self->loaded;
+
+    $self->loaded(1);
+    return 1;
+}
+
+sub unload {
+    my $self = shift;
+
+    return 0 unless $self->loaded;
+
+    $self->loaded(0);
+    return 1;
+}
+
+sub said {
+    my $self = shift;
+    my $args = shift;
+}
 
 1;
+
 
