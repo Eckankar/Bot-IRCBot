70a895707eac145d4b62e6845c46cb7c02845c44
Sebastian Paaske Tørholm <Eckankar+GitHub@gmail.com>
2009-06-19 17:57:04 -0400

    Add files for modular structure.
    new file:   lib/Bot/IRCBot/Module.pm
    new file:   lib/Bot/IRCBot/Modules/YouTube.pm

---
+++ lib/Bot/IRCBot/Module.pm
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+package Bot::IRCBot::Module;
+use Moose;
+
+# This will be an interface for a module.
+
+1;
+
---
+++ lib/Bot/IRCBot/Modules/YouTube.pm
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+package Bot::IRCBot::Modules::YouTube;
+use Moose;
+
+extends 'Bot::IRCBot::Module';
+
+1;
+
