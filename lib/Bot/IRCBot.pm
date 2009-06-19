9f5eab0ccb1dd0cc59962f285bf5bae7b070557c
Sebastian Paaske Tørholm <Eckankar+GitHub@gmail.com>
2009-06-19 19:03:23 -0400

    Added loading of modules

--- lib/Bot/IRCBot.pm
+++ lib/Bot/IRCBot.pm
@@ -15,5 +15,41 @@ do {
     }
 };
 
+has plugins => (
+    metaclass   => 'Collection::List',
+    is          => 'rw',
+    isa         => 'ArrayRef',
+    default     => sub { [] },
+);
+
+sub init {
+    my $self = shift;
+
+    foreach my $plugin (conf->{plugins}) {
+        use "Bot::IRCBot::Plugins::$plugin";
+        push $self->plugins, Bot::IRCBot::Plugins::$plugin->new();
+    }
+
+    foreach my $plugin ($self->plugins) {
+        $plugin->load;
+    }
+}
+
+sub said {
+    my $self = shift;
+    my @output = ();
+
+
+    foreach my $plugin (conf->{plugins}) {
+        next unless $plugin->loaded;
+
+        my $ret = $plugin->said(@_);
+
+        push @output, $ret if $ret;
+    }
+
+    return join "\n", @output;
+}
+
 1;
 
