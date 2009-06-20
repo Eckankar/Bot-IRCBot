02ee406fad7805f2e22842f9f9ddc683197b4518
Sebastian Paaske Tørholm <Eckankar+GitHub@gmail.com>
2009-06-19 20:36:40 -0400

    Add actual functionality to YouTube plugin. modified:   lib/Bot/IRCBot/Modules/YouTube.pm

--- lib/Bot/IRCBot/Modules/YouTube.pm
+++ lib/Bot/IRCBot/Modules/YouTube.pm
@@ -2,8 +2,14 @@
 package Bot::IRCBot::Modules::YouTube;
 use Moose;
 use Carp::Assert;
+use XML::Smart;
 
 extends 'Bot::IRCBot::Module';
+
+use constant {
+    DEVKEY => "AI39si7Go6tZquV3D4E-8_J-DsF81lJkoeOhgtw4RFmCdrvQ".
+              "PYzrZW7E57u_ZJ_IjCggDu6kIyNWGPQ7u0H-0_pQol-I6xRPvg"
+};
 
 has 'name' => (
     is      => 'ro',
@@ -23,9 +29,22 @@ sub said {
     return undef
         unless $body =~ /youtube\.\w{2,3}\S+v=([\w-]+)/;
 
-    my $videoid = $1;
+    my $video_id = $1;
 
-    return "Matched YouTube link; video id: $videoid"
+    my $xml = XML::Smart->new(
+        "http://gdata.youtube.com/feeds/api/videos/$video_id"
+    );
+
+
+    my $entry = $xml->{entry};
+
+    return sprintf(
+        "YouTube: \"%s\" (uploaded by: %s) (avg rating %.2f after %d votes)",
+        $entry->{title}->content,
+        $entry->{author}{name}->content,
+        $entry->{"gd:rating"}{average},
+        $entry->{"gd:rating"}{numRaters}
+    );
 }
 
 1;
